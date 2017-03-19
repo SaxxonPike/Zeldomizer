78a67822d82cc8b1a84d50c39cb3f609268e025d
Anthony Konzel <saxxonpike@gmail.com>
2017-03-19 16:19:14 -0500

    Add Breadbox external

---
+++ Zeldomizer/Engine/Bank0CodeBlock.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zeldomizer.Metal;
+
+namespace Zeldomizer.Engine
+{
+    public class Bank0CodeBlock : CodeBlock
+    {
+        private static readonly IEnumerable<int> StaticAnalysisPoints = new[]
+        {
+            0x9825,
+            0xBF50
+        };
+
+        public Bank0CodeBlock(IRom source, int offset, int length, int codeOffset, IEnumerable<int> staticAnalysisPoints)
+            : base(source, offset, length, codeOffset, staticAnalysisPoints)
+        {
+        }
+    }
+}
---
+++ Zeldomizer/Metal/CodeBlock.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zeldomizer.Metal
+{
+    public class CodeBlock
+    {
+        public CodeBlock(
+            IRom source, 
+            int offset, 
+            int length, 
+            int codeOffset,
+            IEnumerable<int> staticAnalysisPoints)
+        {
+            
+        }
+    }
+}
