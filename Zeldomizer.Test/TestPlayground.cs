da0825a4b582fd2f45aa19246b21bedbe2778b01
Tony Konzel <saxxonpike@gmail.com>
2017-03-29 17:28:40 -0500

    WIP

--- Zeldomizer.Test/TestPlayground.cs
+++ Zeldomizer.Test/TestPlayground.cs
@@ -9,84 +9,9 @@ namespace Zeldomizer
     public class TestPlayground : BaseTestFixture
     {
         [Test]
-        public void Test2()
-        {
-            var cart = new ZeldaCartridge(Source);
-            var rooms = cart.Dungeons.Rooms.ToArray();
-            var compiler = new DungeonCompiler();
-
-            var output = compiler.Compile(rooms);
-            Source.Write(output.Columns.Select(s => unchecked((byte)s)).ToArray(), 0);
-            var rawColumn = new DungeonColumnLibrary(Source, output.ColumnOffsets.Count());
-            var columnOut = rawColumn.ToArray().Select(dc => dc.ToArray());
-
-            foreach (var column in columnOut)
-            {
-                foreach (var tile in column)
-                    Console.Write($"{tile:X1}");
-                Console.WriteLine();
-            }
-        }
-
-        private static char GetMapChar(int input)
-        {
-            var output = '.';
-            switch (input)
-            {
-                case 0:
-                    output = 'X';
-                    break;
-                case 1:
-                    output = '.';
-                    break;
-                case 2:
-                case 3:
-                    output = '&';
-                    break;
-                case 4:
-                    output = 'v';
-                    break;
-                case 5:
-                    output = ',';
-                    break;
-                case 6:
-                    output = '~';
-                    break;
-                case 7:
-                    output = ' ';
-                    break;
-            }
-            return output;
-        }
-
-        private void OutputMap(IReadOnlyList<int> data)
-        {
-            var i = 0;
-            for (var y = 0; y < 7; y++)
-            {
-                for (var x = 0; x < 12; x++)
-                {
-                    Console.Write(GetMapChar(data[i++]));
-                }
-                Console.WriteLine();
-            }
-        }
-
-        [Test]
         public void Test1()
         {
-            var cart = new ZeldaCartridge(Source);
-            var rooms = cart.Dungeons.Rooms.ToArray();
-            var roomNr = 0;
-
-            foreach (var room in rooms)
-            {
-                Console.WriteLine($"Room {roomNr:X2}");
-                roomNr++;
-                var data = room.ToArray();
-                OutputMap(data);
-            }
+            
         }
-
     }
 }
---
+++ Zeldomizer/Metal/OverlappingSourceReader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Zeldomizer.Metal
+{
+    public class OverlappingSourceReader : IEnumerable<ISource>
+    {
+        private readonly ISource _source;
+        private readonly int _count;
+
+        public OverlappingSourceReader(ISource source, int count)
+        {
+            _source = source;
+            _count = count;
+        }
+
+        private IEnumerable<ISource> GetSources()
+        {
+            var i = 0;
+            var remaining = _count;
+            while (remaining > 0)
+            {
+                var input = _source[i];
+                if ((input & 0x80) != 0)
+                {
+                    remaining--;
+                    yield return new SourceBlock(_source, i);
+                }
+                i++;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        public IEnumerator<ISource> GetEnumerator() => GetSources().GetEnumerator();
+    }
+}
