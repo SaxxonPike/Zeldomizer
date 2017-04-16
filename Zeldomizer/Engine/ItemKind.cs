c903d3a24ee7b05517224bd71e2b01064165b0d6
Anthony Konzel <saxxonpike@gmail.com>
2017-04-15 23:30:46 -0500

    Add a nifty item kind enum.

---
+++ Zeldomizer/Engine/ItemKind.cs
@@ -0,0 +1,183 @@
+﻿namespace Zeldomizer.Engine
+{
+    /// <summary>
+    /// Item IDs.
+    /// </summary>
+    public enum ItemKind
+    {
+        /// <summary>
+        /// ID for bombs.
+        /// </summary>
+        Bomb = 0x00,
+
+        /// <summary>
+        /// ID for the wooden sword.
+        /// </summary>
+        Sword = 0x01,
+
+        /// <summary>
+        /// ID for the white sword.
+        /// </summary>
+        WhiteSword = 0x02,
+
+        /// <summary>
+        /// ID for the magic sword.
+        /// </summary>
+        MagicSword = 0x03,
+
+        /// <summary>
+        /// ID for the meat.
+        /// </summary>
+        Meat = 0x04,
+
+        /// <summary>
+        /// ID for the magic whistle.
+        /// </summary>
+        Whistle = 0x05,
+
+        /// <summary>
+        /// ID for the blue candle.
+        /// </summary>
+        BlueCandle = 0x06,
+
+        /// <summary>
+        /// ID for the red candle.
+        /// </summary>
+        RedCandle = 0x07,
+
+        /// <summary>
+        /// ID for regular arrows.
+        /// </summary>
+        Arrow = 0x08,
+
+        /// <summary>
+        /// ID for silver arrows.
+        /// </summary>
+        SilverArrow = 0x09,
+
+        /// <summary>
+        /// ID for the bow.
+        /// </summary>
+        Bow = 0x0A,
+
+        /// <summary>
+        /// ID for the master key.
+        /// </summary>
+        MasterKey = 0x0B,
+
+        /// <summary>
+        /// ID for the raft.
+        /// </summary>
+        Raft = 0x0C,
+
+        /// <summary>
+        /// ID for the ladder.
+        /// </summary>
+        Ladder = 0x0D,
+
+        /// <summary>
+        /// ID for the 5 rupee enemy drop.
+        /// </summary>
+        FiveRupees = 0x0F,
+
+        /// <summary>
+        /// ID for the magic rod.
+        /// </summary>
+        Rod = 0x10,
+
+        /// <summary>
+        /// ID for the book.
+        /// </summary>
+        Book = 0x11,
+
+        /// <summary>
+        /// ID for the blue ring.
+        /// </summary>
+        BlueRing = 0x12,
+
+        /// <summary>
+        /// ID for the red ring.
+        /// </summary>
+        RedRing = 0x13,
+
+        /// <summary>
+        /// ID for the power bracelet.
+        /// </summary>
+        Bracelet = 0x14,
+
+        /// <summary>
+        /// ID for the blue letter.
+        /// </summary>
+        BlueLetter = 0x15,
+
+        /// <summary>
+        /// ID for the compass.
+        /// </summary>
+        Compass = 0x16,
+
+        /// <summary>
+        /// ID for the red letter.
+        /// </summary>
+        RedLetter = 0x17,
+
+        /// <summary>
+        /// ID for a single rupee enemy drop.
+        /// </summary>
+        Rupee = 0x18,
+
+        /// <summary>
+        /// ID for a dungeon key.
+        /// </summary>
+        Key = 0x19,
+
+        /// <summary>
+        /// ID for a heart container.
+        /// </summary>
+        HeartContainer = 0x1A,
+
+        /// <summary>
+        /// ID for a piece of the triforce.
+        /// </summary>
+        Triforce = 0x1B,
+
+        /// <summary>
+        /// ID for the large shield.
+        /// </summary>
+        Shield = 0x1C,
+
+        /// <summary>
+        /// ID for the red boomerang.
+        /// </summary>
+        RedBoomerang = 0x1D,
+
+        /// <summary>
+        /// ID for the blue boomerang.
+        /// </summary>
+        BlueBoomerang = 0x1E,
+
+        /// <summary>
+        /// ID for the blue potion.
+        /// </summary>
+        BluePotion = 0x1F,
+
+        /// <summary>
+        /// ID for the red potion.
+        /// </summary>
+        RedPotion = 0x20,
+
+        /// <summary>
+        /// ID for the clock enemy drop.
+        /// </summary>
+        Clock = 0x21,
+
+        /// <summary>
+        /// ID for a single heart.
+        /// </summary>
+        Heart = 0x22,
+
+        /// <summary>
+        /// ID for a fairy.
+        /// </summary>
+        Fairy = 0x23
+    }
+}
