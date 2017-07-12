b705b8438f3b20dc71e0061b51b8ffe4293f166f
Anthony Konzel <saxxonpike@gmail.com>
2017-07-11 21:21:20 -0500

    Write scaffold for the mapper.

---
+++ Zeldomizer/Engine/AI/UnderworldMapper.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Zeldomizer.Engine.Underworld.Interfaces;
+using Zeldomizer.Metal;
+
+namespace Zeldomizer.Engine.AI
+{
+    public class UnderworldMapper
+    {
+        private readonly IUnderworld _underworld;
+
+        public UnderworldMapper(IUnderworld underworld)
+        {
+            _underworld = underworld;
+        }
+    }
+}
